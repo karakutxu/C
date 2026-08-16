526fc13d68279334020b971ebb13ce6a0f2a62b8
karakutxu <aggie.message@gmail.com>
2026-08-16 10:49:57 +0100

    Delete test_code_ver4 directory

--- test_code_ver4/proto_ng.c
+++
@@ -1,8 +0,0 @@
-#include "proto_ng.h"
-
-void send_error_msg_pkt(struct ifnet *ifp, uint16_t err, proto_context_t *ctx)
-{
-    (void)ifp;
-    (void)err;
-    (void)ctx;
-}
--- test_code_ver4/time_compat.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIME_COMPAT_H
-#define TIME_COMPAT_H
-
-#include <stdint.h>
-
-uint32_t nanotime(uint32_t *secs);
-
-#endif /* TIME_COMPAT_H */
